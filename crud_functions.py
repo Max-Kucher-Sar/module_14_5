c21a1cf22bbfd9c28a9288ac5561ee9eb847e8d9
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2024-12-23 22:20:03 +0400

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -19,11 +19,11 @@ def initiate_db():
 
 def add_user(username, email, age):
     cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
-                   (username, email, age, 1000))
+                   (str(username), str(email), int(age), 1000))
     connection.commit()
 
 def is_included(username):
-    check_user = cursor.execute("SELECT * FROM Users WHERE username = ?", (username))
+    check_user = cursor.execute("SELECT * FROM Users WHERE username = ?", (username, ))
     if check_user.fetchone() is None:
         return True
     else:
